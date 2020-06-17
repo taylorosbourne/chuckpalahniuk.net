d0e71813a661b837a310ccf0088f0bceea19e812
Taylor Osbourne <taylorgosbourne@gmail.com>
2020-06-17 13:17:21 -0400

    formatted

--- cypress/integration/route.spec.ts
+++ cypress/integration/route.spec.ts
@@ -1,11 +1,11 @@
 describe('Home page to About page', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
+	beforeEach(() => {
+		cy.visit('/');
+	});
 
-  it('should navigate to About page and show bio', () => {
-		cy.get('li').contains('Chuck')
-		cy.get('[cy-data="about"]').click()
-    cy.get('h2').contains('Bio')
-  })
-})
+	it('should navigate to About page and show bio', () => {
+		cy.get('li').contains('Chuck');
+		cy.get('[cy-data="about"]').click();
+		cy.get('h2').contains('Bio');
+	});
+});
