3c46a13d36c709e980f805c28e66b55046764d9d
Taylor Osbourne <taylorgosbourne@gmail.com>
2020-06-17 15:35:24 -0400

    formatted

--- src/pages/contact/contact-styles.ts
+++ src/pages/contact/contact-styles.ts
@@ -44,22 +44,25 @@ export const BannerHeader = styled.header`
 
 export const Form = styled.form`
 	width: 70%;
-  margin: 0 auto;
+	margin: 0 auto;
 	padding: 0;
-	* { width:100%; box-sizing: border-box;}
+	* {
+		width: 100%;
+		box-sizing: border-box;
+	}
 	*,
 	*::before,
 	*::after {
-  	box-sizing: border-box;
+		box-sizing: border-box;
 	}
 	label {
 		font-size: 2rem;
 		margin: 5px 0;
 		padding: 10px 0 0 0;
 	}
-  @media(max-width: 1100px) {
-    max-width: 90%;
-  }
+	@media (max-width: 1100px) {
+		max-width: 90%;
+	}
 `;
 
 export const Input = styled.input`
@@ -67,7 +70,7 @@ export const Input = styled.input`
 	margin: 5px 0;
 	border: 0;
 	border-bottom: 3px solid rgb(105, 128, 221);
-  padding: 10px;
+	padding: 10px;
 	background: none;
 	color: #fff;
 `;
@@ -78,41 +81,41 @@ export const Textarea = styled.textarea`
 	color: #fff;
 	border: 0;
 	border-radius: 5px;
-  font-size: 1.35rem;
-  height: 175px;
-  padding: 10px;
+	font-size: 1.35rem;
+	height: 175px;
+	padding: 10px;
 `;
 
 export const SubmitButton = styled.input`
 	display: block;
 	background: rgb(42, 44, 53);
-  color: rgb(105, 128, 221);
-  font-family: 'Bebas Neue', sans-serif;
-  margin: 15px 0;
-  padding: 10px;
-  border: 0;
-  border-left: 3px solid rgb(105, 128, 221);
-  border-bottom: 3px solid rgb(105, 128, 221);
-  border-radius: 5px;
-  cursor: pointer;
+	color: rgb(105, 128, 221);
+	font-family: 'Bebas Neue', sans-serif;
+	margin: 15px 0;
+	padding: 10px;
+	border: 0;
+	border-left: 3px solid rgb(105, 128, 221);
+	border-bottom: 3px solid rgb(105, 128, 221);
+	border-radius: 5px;
+	cursor: pointer;
 	text-align: center;
 	font-size: 2rem;
-  :focus {
-    outline: none;
-    animation: push 0.3s linear;
-  }
-  @keyframes push {
-    0% {
-      border-left: 3px solid rgb(105, 128, 221);
-      border-bottom: 3px solid rgb(105, 128, 221);
-    }
-    50% {
-      border: 0;
-      transform: translate(-3px, 3px);
-    }
-    100% {
-      border-left: 3px solid rgb(105, 128, 221);
-      border-bottom: 3px solid rgb(105, 128, 221);
-    }
-  }
+	:focus {
+		outline: none;
+		animation: push 0.3s linear;
+	}
+	@keyframes push {
+		0% {
+			border-left: 3px solid rgb(105, 128, 221);
+			border-bottom: 3px solid rgb(105, 128, 221);
+		}
+		50% {
+			border: 0;
+			transform: translate(-3px, 3px);
+		}
+		100% {
+			border-left: 3px solid rgb(105, 128, 221);
+			border-bottom: 3px solid rgb(105, 128, 221);
+		}
+	}
 `;
